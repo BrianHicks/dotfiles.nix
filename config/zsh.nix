e4d865374554f97771307581ba29bcf452f284f0
Brian Hicks <brian@brianthicks.com>
2018-08-27 20:51:44 -0500

    add emoji prompt

--- config/zsh.nix
+++ config/zsh.nix
@@ -7,20 +7,25 @@
     dotDir = ".config/zsh";
 
     enableAutosuggestions = true;
-
     enableCompletion = true;
     # TODO: environment.pathsToLink (see home-configuration.nix man page)
+
+    initExtra = ''
+      # from https://gist.github.com/oshybystyi/2c30543cd48b2c9ecab0
+      EMOJI=(🐦 🚀 🎨 🍕 🐭 ☕️ 🔬 🐷 🐼 🐶 🐧 🐳 🍔 🍻 🔮 💰 💎 💾 💜 🍪 🌞 🌍 🐌 🐓 🐏 ✈️ )
+
+      function random_emoji {
+        echo -n "$EMOJI[$RANDOM%$#EMOJI+1]"
+      }
+
+      PROMPT="$(random_emoji)  %c ~ "
+    '';
 
     history = {
       # TODO: why do I need both save and size?
       save = 10000;
       size = 10000;
       share = true;
-    };
-
-    oh-my-zsh = {
-      enable = true;
-      theme = "sunrise";
     };
   };
 }
