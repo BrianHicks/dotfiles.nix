664ef7df085eabe32db9b81ab146d4aa7abd4854
Brian Hicks <brian@brianthicks.com>
2018-08-27 19:52:31 -0500

    enable more cool stuff

--- macbook.nix
+++ macbook.nix
@@ -5,8 +5,36 @@
     ./config/git.nix
   ];
 
+  programs.man.enable = true;
+
+  programs.ssh = {
+    enable = true;
+
+    controlMaster = "auto";
+    controlPath = "~/.ssh/control/%r@%h:%p";
+    controlPersist = "5m";
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    dotDir = ".config/zsh";
+
+    enableAutosuggestions = true;
+
+    enableCompletion = true; # TODO: fzf
+    # TODO: environment.pathsToLink
+
+    history = {
+      # TODO: why do I need both save and size?
+      save = 10000;
+      size = 10000;
+      share = true;
+    };
+  };
+
   programs.home-manager = {
     enable = true;
-    path = "./home-manager";
+    path = "$HOME/dotfiles.nix/home-manager";
   };
 }
