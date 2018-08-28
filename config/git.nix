9fd32692c3de6f9280f5ea426714ad1b3979c7c0
Brian Hicks <brian@brianthicks.com>
2018-08-27 20:27:57 -0500

    add extra config

--- config/git.nix
+++ config/git.nix
@@ -9,13 +9,25 @@
 
     aliases = {
       aa = "add --all";
+      amend = "commit --amend";
       ci = "commit";
+      co = "checkout";
       dc = "diff --cached";
       di = "diff";
+      publish = "!git push -u origin $(git rev-parse --abbrev-ref HEAD)";
       st = "status";
     };
 
-    # todo: extraConfig with lfs
+    extraConfig = {
+      # REmember REbase REsults
+      rerere.enabled = true;
+
+      # LFS
+      "filter \"lfs\"".clean = "git-lfs clean -- %f";
+      "filter \"lfs\"".smudge = "git-lfs smudge -- %f";
+      "filter \"lfs\"".process = "git-lfs filter-process";
+      "filter \"lfs\"".required = true;
+    };
 
     # todo: global ignores
   };
