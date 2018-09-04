c74ce969297c2fe1361372479a1d6887e3840ce6
Brian Hicks <brian@brianthicks.com>
2018-09-04 13:52:00 -0500

    use Hack instead of Fira Code

--- config/emacs/display.el
+++ config/emacs/display.el
@@ -68,7 +68,7 @@
 
 ;; fonts and ligatures
 (when (window-system)
-  (set-face-attribute 'default nil :height 130 :family "Fira Code")
+  (set-face-attribute 'default nil :height 130 :family "Hack")
   (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
 
 ;; transparent title bar
