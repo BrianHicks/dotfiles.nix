a98c55c6e9daa09c64ad037256cafab8a7ff2974
Brian Hicks <brian@brianthicks.com>
2018-09-22 13:57:15 -0500

    patch evil-escape

--- config/emacs.nix
+++ config/emacs.nix
@@ -13,10 +13,21 @@ let
     {
       file = ./emacs/evil.el;
       pkgs = epkgs: [
+        # there's a bug in the current source of evil-escape that causes it to
+        # fail to build. We'll patch it out for now and hope it gets fixed in a
+        # future version.
+        (epkgs.evil-escape.overrideAttrs (attrs: {
+          patches = (attrs.patches or []) ++ [
+            (pkgs.fetchpatch {
+              url = https://github.com/BrianHicks/evil-escape/commit/b548e8450570a0c8dea47b47221b728c047a9baf.patch;
+              sha256 = "1a2qrf4bpj7wm84qa3haqdg3pd9d8nh5vrj8v1sc0j1a9jifsbf6";
+            })
+          ];
+        }))
+
         epkgs.evil
         epkgs.evil-commentary
         epkgs.evil-ediff
-        epkgs.evil-escape
         epkgs.evil-exchange
         epkgs.evil-matchit
         epkgs.evil-surround
