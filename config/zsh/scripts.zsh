295844a1da419cfa7b9186a9098a20361eafddd5
Brian Hicks <brian@brianthicks.com>
2018-10-05 05:50:23 -0500

    remove deprecated syntax in find

--- config/zsh/scripts.zsh
+++ config/zsh/scripts.zsh
@@ -1,7 +1,7 @@
 # running scripts in projects with `script/` as a directory full of executables
 run_script() {
     SCRIPT_DIR=script
-    SELECTED=$(find "$SCRIPT_DIR" -perm +111 -type f | sed "s|$SCRIPT_DIR/||g" | fzf --select-1 --query="$1")
+    SELECTED=$(find "$SCRIPT_DIR" -perm /111 -type f | sed "s|$SCRIPT_DIR/||g" | fzf --select-1 --query="$1")
 
     if [[ "$?" != 0 ]]; then echo "cancelling!"; return 1; fi
 
