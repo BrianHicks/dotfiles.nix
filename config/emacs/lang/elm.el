064a3f62c425a828b00611a79b4a5eb9364559d5
Brian Hicks <brian@brianthicks.com>
2019-02-05 16:29:00 -0600

    steal neat functions from Juan

--- config/emacs/lang/elm.el
+++ config/emacs/lang/elm.el
@@ -15,9 +15,12 @@
                  "cc" 'elm-compile-buffer
 
                  "e" '(:ignore t :which-key "edit")
-                 "ea" 'elm-compile-add-annotations
-                 "eI" 'elm-compile-clean-imports
-                 "ef" 'elm-mode-format-buffer)
+                 "ei" 'elm-extra/import
+                 "eI" 'elm-extra/import-from-file
+                 "ef" 'elm-mode-format-buffer
+
+                 "y" '(:ignore t :which-key "yank")
+                 "ym" 'elm-extra/show-and-copy-module-name)
 
   :config
   (setq elm-tags-on-save t
@@ -62,6 +65,53 @@
            (lambda (component) (string-equal component (downcase component)))
            components))
         )
+    (string-join modules ".")))
+
+
+;;; extra stuff from Juan: https://github.com/juanedi/.spacemacs.d/blob/master/layers/elm-extra/funcs.el
+
+(defun elm-extra/import (&optional input)
+  "Prompt for an import statement (INPUT) to add to the current file."
+  (interactive)
+  (let ((statement (read-string "Import statement: " (concat "import " input))))
+    (save-excursion
+      (goto-char (point-min))
+      (if (re-search-forward "^import " nil t)
+          (beginning-of-line)
+        (forward-line 1)
+        (insert "\n"))
+      (insert (concat statement "\n")))
+    (elm-sort-imports)))
+
+(defun elm-extra/import-from-file ()
+  "Select an elm file interactively and add an import for the corresponding module."
+  (interactive)
+  (let*
+      ((all-files (projectile-current-project-files))
+       (elm-files (seq-filter (lambda (f) (s-ends-with-p ".elm" f)) all-files))
+       (file-name (projectile-completing-read "Module to import: " elm-files)))
+    (when file-name
+      (let*
+          ((full-file-name (expand-file-name file-name (projectile-project-root)))
+           (module-name (with-current-buffer (find-file-noselect full-file-name)
+                          (elm--get-module-name))))
+        (elm-extra/import module-name)))))
+
+(defun elm-extra/show-and-copy-module-name ()
+  "Show and copy the current module's name in the minibuffer."
+  (interactive)
+  (message (kill-new (elm--get-module-name))))
+
+(defun elm-extra/current-module-name ()
+  "Get the module name of the current buffer."
+  (let*
+      ((raw-components
+        (file-name-sans-extension (file-relative-name (buffer-file-name) (elm-test-runner--project-root))))
+       (components
+        (split-string raw-components "/"))
+       (modules
+        (remove-if (lambda (c) (string-equal c (downcase c)))
+                   components)))
     (string-join modules ".")))
 
 ;;; elm.el ends here
