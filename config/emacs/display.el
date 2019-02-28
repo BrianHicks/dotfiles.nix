af281c96740e78b46b48bfd8b5062dec5551f7d8
Brian Hicks <brian@brianthicks.com>
2019-02-27 20:29:18 -0600

    swap load theme with config to pick up variables first

--- config/emacs/display.el
+++ config/emacs/display.el
@@ -45,8 +45,6 @@
 (use-package airline-themes
   :after powerline
   :config
-  (load-theme 'airline-doom-one t)
-
   (setq airline-helm-colors                   nil
         powerline-utf-8-separator-left        #xe0b0
         powerline-utf-8-separator-right       #xe0b2
@@ -56,7 +54,9 @@
         airline-utf-glyph-subseparator-right  #xe0b3
         airline-utf-glyph-branch              #xe0a0
         airline-utf-glyph-readonly            #xe0a2
-        airline-utf-glyph-linenumber          #xe0a1))
+        airline-utf-glyph-linenumber          #xe0a1)
+
+  (load-theme 'airline-doom-one t))
 
 ;; fonts and ligatures
 (when (window-system)
