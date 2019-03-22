fc2609bcff6f7118fffa115a1331d03f4be2a9a7
Brian Hicks <brian@brianthicks.com>
2019-03-22 15:13:54 -0500

    remove duplicate case in regex

--- config/zsh/elm.zsh
+++ config/zsh/elm.zsh
@@ -3,5 +3,5 @@
 # look at the outline of an Elm file. Useful for seeing if an API is self-describing.
 elm_outline() {
     MODULE=$1
-    grep -E '^(type alias [\w ]+|type [\w ]+|\w+ : .+)' $MODULE
+    grep -E '^(type|--|\w+ : .+)' $MODULE
 }
