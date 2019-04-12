838ad5b5f0f3aede70dd73b3626df96463a7a1d3
Brian Hicks <brian@brianthicks.com>
2019-04-12 05:41:24 -0500

    add lorri to home-manager

--- macbook.nix
+++ macbook.nix
@@ -19,6 +19,11 @@
     pkgs.pv
     pkgs.tree
     pkgs.watch
+
+    # local packages. I know I could use overlays for these (cf
+    # https://github.com/jwoudenberg/dotfiles/commit/12bd31b269b82f0dc661140b8df275ef24f41b81)
+    # but I don't want to have to symlink into the overlays directory manually.
+    (pkgs.callPackage ./pkgs/lorri.nix { })
   ];
 
   programs.man.enable = true;
