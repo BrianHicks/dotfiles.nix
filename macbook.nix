484aac5eef3a2d7e45057e63a52e54c3d9dec83e
Brian Hicks <brian@brianthicks.com>
2019-04-13 05:59:09 -0500

    use use nix-darwin to manage stuff (WIP)

--- macbook.nix
+++ macbook.nix
@@ -1,37 +1,34 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  home.stateVersion = "18.09";
+  imports = [ ./home-manager/nix-darwin ];
 
-  imports = [
-    ./config/direnv.nix
-    ./config/emacs.nix
-    ./config/fzf.nix
-    ./config/git.nix
-    ./config/kakoune.nix
-    ./config/ssh.nix
-    ./config/tmux.nix
-    ./config/zsh.nix
-  ];
+  environment.systemPackages = [];
 
-  home.packages = [
-    pkgs.ag
-    pkgs.awscli
-    pkgs.jq
-    pkgs.pv
-    pkgs.tree
-    pkgs.watch
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/dotfiles.nix/macbook.nix";
 
-    # local packages. I know I could use overlays for these (cf
-    # https://github.com/jwoudenberg/dotfiles/commit/12bd31b269b82f0dc661140b8df275ef24f41b81)
-    # but I don't want to have to symlink into the overlays directory manually.
-    (pkgs.callPackage ./pkgs/lorri.nix { })
-  ];
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-  programs.man.enable = true;
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.bash.enable = true;
+  # programs.zsh.enable = true;
+  # programs.fish.enable = true;
 
-  programs.home-manager = {
-    enable = true;
-    path = "$HOME/dotfiles.nix/home-manager";
-  };
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 3;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 4;
+  nix.buildCores = 4;
+
+  users.users.brianhicks.name = "brianhicks";
+  users.users.brianhicks.home = "/Users/brianhicks";
+  home-manager.useUserPackages = true;
+  home-manager.users.brianhicks = (import ./macbook-hm.nix);
 }
