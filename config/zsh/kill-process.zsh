649b38402b27c37b16e76b3e3b961a0827ea5df7
Brian Hicks <brian@brianthicks.com>
2019-05-30 12:22:12 -0500

    show all processes but use stricter matching

--- config/zsh/kill-process.zsh
+++ config/zsh/kill-process.zsh
@@ -1,7 +1,7 @@
 # killing processes
 # ripped off https://github.com/SidOfc/dotfiles/blob/d07fa3862ed065c2a5a7f1160ae98416bfe2e1ee/zsh/kp
 kill-process() {
-    local PROCESSES="$(ps a | sed 1d | fzf -m | awk '{print $1}')"
+    local PROCESSES="$(ps aux | sed 1d | fzf -em | awk '{print $1}')"
 
     if test -n "$PROCESSES"; then
         echo "$PROCESSES" | xargs kill -${1:-9}
