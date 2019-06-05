bbd4163626fdd70b14c9fd3ead29e875886dce4e
Brian Hicks <brian@brianthicks.com>
2019-06-04 22:28:29 -0700

    add a kakrc with nicer defaults

--- config/kakoune.nix
+++ config/kakoune.nix
@@ -4,4 +4,11 @@
   home.packages = [
     pkgs.kakoune
   ];
+
+  home.file.".config/kak/kakrc" = {
+    text = ''
+      colorscheme gruvbox
+      add-highlighter global/ wrap -word
+    '';
+  };
 }
