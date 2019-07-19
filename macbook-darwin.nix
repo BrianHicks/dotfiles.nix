c0975bde5403c0c46dfc0a4295aabbe57ae40637
Brian Hicks <brianhicks@flame.local>
2019-07-18 21:51:21 -0500

    reset to default darwin config

--- macbook-darwin.nix
+++ macbook-darwin.nix
@@ -1,45 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./home-manager/nix-darwin ];
-
-  environment.systemPackages = [];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.emacs
+    ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
 
-  # set NIX_PATH without the root user
-  nix.nixPath = [
-    "darwin-config=$HOME/dotfiles.nix/macbook.nix"
-    "$HOME/.nix-defexpr/channels"
-  ];
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  # for some reason I need to add /run/current-system/sw/bin/zsh as the login
-  # shell for iterm. It *looks* like it should be setting the login shell but
-  # it doesn't seem to be taking effect.
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.bash.enable = true;
   programs.zsh.enable = true;
+  # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 3;
+  system.stateVersion = 4;
 
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
-  nix.maxJobs = 4;
-  nix.buildCores = 4;
-
-  # I can't use home-manager with nix-darwin right now because Applications are
-  # not linked in global packages so I cannot use graphical emacs.
-  #
-  # See https://github.com/LnL7/nix-darwin/issues/139 (cf.
-  # https://github.com/rycee/home-manager/pull/702) for updates.
-  # home-manager = {
-  #   useUserPackages = true;
-  #   users.brianhicks = (import ./macbook-hm.nix);
-  # };
+  nix.maxJobs = 8;
+  nix.buildCores = 8;
 }
