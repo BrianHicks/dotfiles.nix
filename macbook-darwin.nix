02d3814c58599830f77b857c64e7cc8a04d8d73c
Brian Hicks <brian@brianthicks.com>
2019-07-19 17:15:11 -0500

    use home-manager

--- macbook-darwin.nix
+++ macbook-darwin.nix
@@ -1,32 +1,44 @@
 { config, pkgs, ... }:
 
-{
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = [];
+let
+  macbook = import ./macbook.nix;
+in
+  {
+    imports = [
+      ./home-manager/nix-darwin
+    ];
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.dotfiles/macbook-darwin.nix
-  environment.darwinConfig = "$HOME/dotfiles.nix/macbook-darwin.nix";
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    environment.systemPackages = [];
 
-  # allow zsh as a login shell
-  environment.shells = [ pkgs.zsh ];
+    # Use a custom configuration.nix location.
+    # $ darwin-rebuild switch -I darwin-config=$HOME/.dotfiles/macbook-darwin.nix
+    environment.darwinConfig = "$HOME/dotfiles.nix/macbook-darwin.nix";
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+    # allow zsh as a login shell
+    environment.shells = [ pkgs.zsh ];
 
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  # programs.bash.enable = true;
-  programs.zsh.enable = true;
-  # programs.fish.enable = true;
+    # Auto upgrade nix package and the daemon service.
+    services.nix-daemon.enable = true;
+    # nix.package = pkgs.nix;
 
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+    # Create /etc/bashrc that loads the nix-darwin environment.
+    # programs.bash.enable = true;
+    programs.zsh.enable = true;
+    # programs.fish.enable = true;
 
-  # You should generally set this to the total number of logical cores in your system.
-  # $ sysctl -n hw.ncpu
-  nix.maxJobs = 8;
-  nix.buildCores = 8;
-}
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 4;
+
+    # You should generally set this to the total number of logical cores in your system.
+    # $ sysctl -n hw.ncpu
+    nix.maxJobs = 8;
+    nix.buildCores = 8;
+
+    home-manager = {
+      useUserPackages = true;
+      users.brianhicks = macbook;
+    };
+  }
