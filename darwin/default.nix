7e2ff7b469e8bc9e3b2ce7cb6010280f11a619d8
Brian Hicks <brian@brianthicks.com>
2019-07-23 13:35:50 -0500

    inline dotfiles import

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,46 +1,44 @@
 { config, pkgs, ... }:
 
-let
-  dotfiles = import ../dotfiles;
-in
-  {
-    imports = [
-      ../home-manager/nix-darwin
-      ./lorri.nix
-      ./defaults.nix
-    ];
+{
+  imports = [
+    ../home-manager/nix-darwin
+    ./lorri.nix
+    ./defaults.nix
+  ];
 
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages = [];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [];
 
-    # Use a custom configuration.nix location.
-    # $ darwin-rebuild switch -I darwin-config=$HOME/.dotfiles/darwin/default.nix
-    environment.darwinConfig = "$HOME/dotfiles.nix/darwin/default.nix";
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.dotfiles/darwin/default.nix
+  environment.darwinConfig = "$HOME/dotfiles.nix/darwin/default.nix";
 
-    # allow zsh as a login shell
-    environment.shells = [ pkgs.zsh ];
+  # allow zsh as a login shell
+  environment.shells = [ pkgs.zsh ];
 
-    # Auto upgrade nix package and the daemon service.
-    services.nix-daemon.enable = true;
-    # nix.package = pkgs.nix;
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-    # Create /etc/bashrc that loads the nix-darwin environment.
-    # programs.bash.enable = true;
-    programs.zsh.enable = true;
-    # programs.fish.enable = true;
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.bash.enable = true;
+  programs.zsh.enable = true;
+  # programs.fish.enable = true;
 
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    system.stateVersion = 4;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 
-    # You should generally set this to the total number of logical cores in your system.
-    # $ sysctl -n hw.ncpu
-    nix.maxJobs = 8;
-    nix.buildCores = 8;
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 8;
+  nix.buildCores = 8;
 
-    home-manager = {
-      useUserPackages = true;
-      users.brianhicks = dotfiles;
-    };
-  }
+  # dotfiles
+  home-manager = {
+    useUserPackages = true;
+    users.brianhicks = (import ../dotfiles);
+  };
+}
