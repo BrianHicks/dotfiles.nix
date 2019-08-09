8dd32e0dba6559f50d328917a60ceab785118605
Brian Hicks <brian@brianthicks.com>
2019-08-09 15:39:27 -0500

    add sample stuff from the coc.nvim readme

--- dotfiles/neovim.nix
+++ dotfiles/neovim.nix
@@ -101,6 +101,50 @@ in {
         set noshowmode
 
         " language server
+        set cmdheight=2
+        set updatetime=300
+        set shortmess+=c
+
+        " https://github.com/neoclide/coc.nvim#example-vim-configuration
+        " Use tab for trigger completion with characters ahead and navigate.
+        " Use command ':verbose imap <tab>' to make sure tab is not mapped by other plugin.
+        inoremap <silent><expr> <TAB>
+              \ pumvisible() ? "\<C-n>" :
+              \ <SID>check_back_space() ? "\<TAB>" :
+              \ coc#refresh()
+        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+        function! s:check_back_space() abort
+          let col = col('.') - 1
+          return !col || getline('.')[col - 1]  =~# '\s'
+        endfunction
+
+        " Use <cr> to confirm completion, `<C-g>u` means break undo chain at current position.
+        " Coc only does snippet and additional edit on confirm.
+        inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
+
+        " Use `[c` and `]c` to navigate diagnostics
+        nmap <silent> [c <Plug>(coc-diagnostic-prev)
+        nmap <silent> ]c <Plug>(coc-diagnostic-next)
+        
+        " Remap keys for gotos
+        " TODO: find a way to only call these if the language server is active
+        nmap <silent> gd <Plug>(coc-definition)
+        nmap <silent> gy <Plug>(coc-type-definition)
+        nmap <silent> gi <Plug>(coc-implementation)
+        nmap <silent> gr <Plug>(coc-references)
+
+        " Use K to show documentation in preview window
+        nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+        function! s:show_documentation()
+          if (index(['vim','help'], &filetype) >= 0)
+            execute 'h '.expand('<cword>')
+          else
+            call CocAction('doHover')
+          endif
+        endfunction
+
         let g:coc_global_extensions = [ 'coc-git', 'coc-json', 'coc-yaml', 'coc-html', 'coc-vimlsp' ]
 
         nnoremap <leader>lR :CocRestart<CR>
@@ -111,6 +155,18 @@ in {
         nnoremap <leader>ll :CocList<CR>
         nnoremap <leader>lo :CocOpenLog<CR>
         nnoremap <leader>lr :CocListResume<CR>
+
+        nnoremap <leader>er <Plug>(coc-rename)
+        xmap <leader>ef <Plug>(coc-format)
+        nmap <leader>ef <Plug>(coc-format)
+
+        augroup mygroup
+          autocmd!
+          " Setup formatexpr specified filetype(s).
+          autocmd FileType elm setl formatexpr=CocAction('formatSelected')
+          " Update signature help on jump placeholder
+          autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+        augroup end
 
         " git
         nmap [g <Plug>(coc-git-prevchunk)
