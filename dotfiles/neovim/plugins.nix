bb7a88aaa525d0c3e84390ce82b8d39e00af9e52
Brian Hicks <brian@brianthicks.com>
2019-08-09 10:58:29 -0500

    add a bunch of ruby/rails plugins

--- dotfiles/neovim/plugins.nix
+++ dotfiles/neovim/plugins.nix
@@ -62,6 +62,36 @@ rec {
     };
   };
 
+  tpope.vim-abolish = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-abolish";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-abolish";
+      rev = "b95463a1cffd8fc9aff2a1ff0ae9327944948699";
+      sha256 = "1cvhylz6hgvl63zhlrxqrjqqp07pm29i436xv33dzzhdp8dcj1mp";
+    };
+  };
+
+  tpope.vim-bundler = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-bundler";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-bundler";
+      rev = "c4553a999c77e48bf74f43ac3d3503456bbba5a5";
+      sha256 = "1wmvxisi1afbqmd35yv8cg38p5sg1d1q2nd5g3f4h3ac58v84bl1";
+    };
+  };
+
+  tpope.vim-endwise = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-endwise";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-endwise";
+      rev = "f67d022169bd04d3c000f47b1c03bfcbc4209470";
+      sha256 = "0lq2sphh2mfciva184b4b3if202hr4yls4d2gzbjx7ibch45zb9i";
+    };
+  };
+
   tpope.vim-fugitive = pkgs.vimUtils.buildVimPlugin {
     name = "vim-fugitive";
     src = pkgs.fetchFromGitHub {
@@ -69,6 +99,36 @@ rec {
       repo = "vim-fugitive";
       rev = "bc3d36e559e446d963e0b1dd832d3b39ea233241";
       sha256 = "1072ldcvp1kq5kckzxvr6g6qijf2a6ccrig1v63jydpdqmn8h6kq";
+    };
+  };
+
+  tpope.vim-projectionist = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-projectionist";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-projectionist";
+      rev = "94001f00825c36fab63b9f3ca47a204111e561a2";
+      sha256 = "0pppaavma07c3lxqlbk8cghdsirncxng52mjmv5qk8yar8kxqvbr";
+    };
+  };
+
+  tpope.vim-rails = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-rails";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-rails";
+      rev = "2b33a3df78f337ec44874fa9fb2862ab229614ce";
+      sha256 = "06nhfbj410lbfn1ik3hdg0xd4533xn2d7ir5c4imra211svgyzwx";
+    };
+  };
+
+  tpope.vim-rake = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-rake";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-rake";
+      rev = "673289cc8a620765afb51a884589cd5258b2e396";
+      sha256 = "1mhsn33myajwwkfyf6kspy6gvsx1264f237mdvzprx3npazada0p";
     };
   };
 
@@ -92,6 +152,16 @@ rec {
     };
   };
 
+  tpope.vim-sleuth = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-sleuth";
+    src = pkgs.fetchFromGitHub {
+      owner = "tpope";
+      repo = "vim-sleuth";
+      rev = "7a104e34c10c6f3581c6e98da7834d765d0b067c";
+      sha256 = "0i147vhrrkarir36ysyaic42d22hk38cnpaqzqck7b2zdwnqrvbv";
+    };
+  };
+
   tpope.vim-surround = pkgs.vimUtils.buildVimPlugin {
     name = "vim-surround";
     src = pkgs.fetchFromGitHub {
@@ -112,6 +182,16 @@ rec {
     };
   };
 
+  vim-ruby.vim-ruby = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-ruby";
+    src = pkgs.fetchFromGitHub {
+      owner = "vim-ruby";
+      repo = "vim-ruby";
+      rev = "1aa8f0cd0411c093d81f4139d151f93808e53966";
+      sha256 = "04ng7mjjdacajkmx20pfwlfh1h43sh6sx58id830q9jjl7kvyhhp";
+    };
+  };
+
   all = [
     ElmCast.elm-vim
     LnL7.vim-nix
@@ -119,10 +199,18 @@ rec {
     junegunn."fzf.vim"
     neoclide."coc.nvim"
     rakr.vim-one
+    tpope.vim-abolish
+    tpope.vim-bundler
+    tpope.vim-endwise
     tpope.vim-fugitive
+    tpope.vim-projectionist
+    tpope.vim-rails
+    tpope.vim-rake
     tpope.vim-repeat
     tpope.vim-sensible
+    tpope.vim-sleuth
     tpope.vim-surround
     tpope.vim-unimpaired
+    vim-ruby.vim-ruby
   ];
 }
