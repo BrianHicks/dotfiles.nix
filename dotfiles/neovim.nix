740ba46e7e4c7f563a44248f067a1c91c4017fb6
Brian Hicks <brian@brianthicks.com>
2019-08-09 16:54:47 -0500

    disable breaking line

--- dotfiles/neovim.nix
+++ dotfiles/neovim.nix
@@ -121,7 +121,7 @@ in {
 
         " Use <cr> to confirm completion, `<C-g>u` means break undo chain at current position.
         " Coc only does snippet and additional edit on confirm.
-        inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
+        " inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
 
         " Use `[c` and `]c` to navigate diagnostics
         nmap <silent> [c <Plug>(coc-diagnostic-prev)
