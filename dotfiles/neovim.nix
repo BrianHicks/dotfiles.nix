73e73625a7b6273e49f60eedd249afcb5df6d0b6
Brian Hicks <brian@brianthicks.com>
2019-08-09 09:12:18 -0500

    sort bindings

--- dotfiles/neovim.nix
+++ dotfiles/neovim.nix
@@ -98,11 +98,11 @@ in {
         nmap gs <Plug>(coc-git-chunkinfo)
         nmap gc <Plug>(coc-git-commit)
 
-        nnoremap <leader>gs :Gstatus<CR>
-        nnoremap <leader>go :CocCommand git.browserOpen<CR>
-        nnoremap <leader>gf :CocCommand git.foldUnchanged<CR>
         nnoremap <leader>gC :CocCommand git.copyUrl<CR>
         nnoremap <leader>gd :CocCommand git.diffCached<CR>
+        nnoremap <leader>gf :CocCommand git.foldUnchanged<CR>
+        nnoremap <leader>go :CocCommand git.browserOpen<CR>
+        nnoremap <leader>gs :Gstatus<CR>
 
         nnoremap <leader>gr :Gmove<CR>
         nnoremap <leader>gR :Gremove<CR>
@@ -110,8 +110,8 @@ in {
         nnoremap <leader>gW :Gwrite<CR>
         nnoremap <leader>gX :CocCommand git.chunkUndo<CR>
 
-        nnoremap <leader>gl :CocList commits<CR>
         nnoremap <leader>gb :CocList branches<CR>
+        nnoremap <leader>gl :CocList commits<CR>
       '';
 
       packages.myVimPackage.start = plugins.all ++ [ pkgs.fzf ];
