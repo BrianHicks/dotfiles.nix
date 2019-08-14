5c646b78f0d4f605ff04d610c471e0c4d2035965
Brian Hicks <brian@brianthicks.com>
2019-08-14 15:20:31 -0500

    remove duplicate vim-test entry

--- dotfiles/neovim/plugins.nix
+++ dotfiles/neovim/plugins.nix
@@ -115,16 +115,6 @@ rec {
     };
   };
 
-  janko.vim-test = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-test";
-    src = pkgs.fetchFromGitHub {
-      owner = "janko";
-      repo = "vim-test";
-      rev = "21499f536a004a98a853e54c446ba6128809fdc5";
-      sha256 = "1m509p9jnb0h294vf9xjgf79gjlkxiqzx8sz75kr0msyvpnnyp6j";
-    };
-  };
-
   junegunn."fzf.vim" = pkgs.vimUtils.buildVimPlugin {
     name = "fzf.vim";
     src = pkgs.fetchFromGitHub {
@@ -426,7 +416,6 @@ rec {
     glts.vim-radical
     godlygeek.tabular
     itchyny."lightline.vim"
-    janko.vim-test
     janko.vim-test
     junegunn."fzf.vim"
     kkoomen.vim-doge
