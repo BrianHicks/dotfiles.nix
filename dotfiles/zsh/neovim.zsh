329bf9029934db725092ce0136a3a466cafb483a
Brian Hicks <brian@brianthicks.com>
2019-09-03 13:48:38 -0500

    use proper ignore

--- dotfiles/zsh/neovim.zsh
+++ dotfiles/zsh/neovim.zsh
@@ -2,7 +2,7 @@
 
 find_and_edit() {
   if test -d .git; then
-    SOURCE="$(git ls-files)"
+    SOURCE="$(git ls-files --others --cached --modified --exclude-standard)"
   else
     SOURCE="$(find . -type f)"
   fi
