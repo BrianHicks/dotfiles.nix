e4b125af70ce06d10466f5ae43a9bb088af58a89
Brian Hicks <brian@brianthicks.com>
2019-09-17 16:26:02 -0500

    look in the right place for lorri logs

--- dotfiles/zsh/lorri.zsh
+++ dotfiles/zsh/lorri.zsh
@@ -2,7 +2,7 @@
 
 # wait for lorri to be ready, then reload direnv
 lorri-wait() {
-    while tail -n 1 /tmp/lorri-daemon.log | grep -q Started; do
+    while tail -n 1 /usr/local/var/log/lorri.out.log | grep -q Started; do
         echo "waiting"
         sleep 2
     done
