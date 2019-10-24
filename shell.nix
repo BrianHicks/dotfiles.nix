f7f48b9e188907a05219290ad961d87657c8d5d7
Brian Hicks <brian@brianthicks.com>
2019-10-24 06:15:56 -0500

    add stuff for testing locally

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { };
+  niv = import sources.niv { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "dotfiles";
+  buildInputs = [
+    ## meta-information for managing dotfiles
+    niv.niv
+
+    ## sample projects
+    # elm
+    elmPackages.elm
+    elmPackages.elm-format
+  ];
+}
