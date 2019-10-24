4c444334bddaf4664fcb08c54c63e772952d1871
Brian Hicks <brian@brianthicks.com>
2019-10-24 06:30:56 -0500

    make test-vim work from wherever

--- test-vim.sh
+++ test-vim.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cd $(dirname $(realpath $0))
 make result
 ROOT="$(grep -m 1 'home-manager-generation/activate' result/activate | cut -d ' ' -f 5 | xargs dirname)"
 exec "$ROOT/home-path/bin/vim" $@
