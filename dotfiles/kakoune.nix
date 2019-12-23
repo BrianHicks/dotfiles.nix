8b2a09ca528bd3375ba690945a45c063691ccff3
Brian Hicks <brian@brianthicks.com>
2019-12-23 08:53:47 -0600

    make autoload directory for kakoune

--- dotfiles/kakoune.nix
+++ dotfiles/kakoune.nix
@@ -1,8 +1,15 @@
 { pkgs, lib, ... }:
 let
   sources = import ../nix/sources.nix;
-in
-{
+  nixpkgs = import sources.nixpkgs { };
+  kakoune = import ../lib/kakoune.nix {
+    pkgs = nixpkgs.pkgs;
+    lib = nixpkgs.lib;
+  };
+
+  # plugins
+  plugins = kakoune.mkPlugins [ ];
+in {
   programs.kakoune = {
     enable = true;
     config = {
@@ -12,8 +19,8 @@ in
         lines = 5;
       };
       numberLines = {
-       enable = true;
-       separator = " ";
+        enable = true;
+        separator = " ";
       };
       showMatching = true;
       ui.enableMouse = true;
@@ -27,4 +34,5 @@ in
 
   # plugins
   home.file.".config/kak/colors".source = sources.kakoune-colors;
+  home.file.".config/kak/autoload".source = "${plugins}/share/kak/autload";
 }
