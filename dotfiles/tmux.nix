3a6013be31b75ec2befe7b8527e3900fd28c61a7
Brian Hicks <brian@brianthicks.com>
2019-12-24 06:16:44 -0600

    add a bunch more tmux configuration

--- dotfiles/tmux.nix
+++ dotfiles/tmux.nix
@@ -1,7 +1,19 @@
 { pkgs, lib, ... }: {
   programs.tmux = {
     enable = true;
-    terminal = "xterm-256color";
-    extraConfig = "set -ga terminal-overrides \",*col*:Tc\"";
+    terminal = "tmux-256color";
+    shortcut = "a";
+    extraConfig = ''
+      set -ga terminal-overrides ",*col*:Tc"
+
+      # navigate around split panes with C-{h/j/k/l}
+      bind-key -n C-h select-pane -L
+      bind-key -n C-j select-pane -U
+      bind-key -n C-k select-pane -D
+      bind-key -n C-l select-pane -R
+
+      # get rid of the half-second escape time for kakoune's escape key
+      set -sg escape-time 25
+    '';
   };
 }
