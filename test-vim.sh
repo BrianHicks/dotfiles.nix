b5481b5e21be3939d56efd055a8962ac6cb3fed0
Brian Hicks <brian@brianthicks.com>
2020-03-18 09:07:39 -0500

    remove test-vim.sh

--- test-vim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd $(dirname $(realpath $0))
-make result
-ROOT="$(grep -m 1 'home-manager-generation/activate' result/activate | cut -d ' ' -f 5 | xargs dirname)"
-
-cd -
-exec "$ROOT/home-path/bin/vim" $@
