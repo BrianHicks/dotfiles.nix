518076ca4c3798aca3072e106b37c9cfaabdcd62
Brian Hicks <brian@brianthicks.com>
2020-05-20 23:12:49 -0500

    add ayu colors

--- pkgs/kak-ayu/index.js
+++ pkgs/kak-ayu/index.js
@@ -16,50 +16,61 @@ function rgb(color) {
     `ayu-${theme.name}.kak`,
     `\
 # Code highlighting
-face global value     ${rgb(colors.syntax.constant)}
-face global type      ${rgb(colors.syntax.tag)}+b
+face global value     ${rgb(colors.syntax.markup)}
+face global type      ${rgb(colors.syntax.entity)}+b
 face global variable  ${rgb(colors.common.fg)}
-face global module    ${rgb(colors.syntax.tag)}
+face global module    ${rgb(colors.syntax.entity)}+b
 face global function  ${rgb(colors.syntax.func)}
 face global string    ${rgb(colors.syntax.string)}
 face global keyword   ${rgb(colors.syntax.keyword)}
 face global operator  ${rgb(colors.syntax.operator)}
-face global attribute ${rgb(colors.syntax.keyword)}
-face global comment   ${rgb(colors.syntax.comment)}
-face global meta      ${rgb(colors.syntax.comment)}
-face global builtin   ${rgb(colors.syntax.func)}+b
+face global attribute ${rgb(colors.syntax.operator)}
+face global comment   ${rgb(colors.syntax.comment)}+i
+face global meta      ${rgb(colors.syntax.special)}
+face global builtin   ${rgb(colors.syntax.tag)}+b
 
 # Markdown highlighting
-face global title     ${rgb(colors.syntax.func)}+b
+face global title     ${rgb(colors.syntax.tag)}+b
 face global header    ${rgb(colors.syntax.tag)}
 face global bold      ${rgb(colors.common.fg)}+b
 face global italic    ${rgb(colors.common.fg)}+i
-face global mono      ${rgb(colors.common.fg)}
+face global mono      ${rgb(colors.syntax.string)}
 face global block     default
-face global link      default
-face global bullet    default
-face global list      default
+face global link      ${rgb(colors.syntax.markup)}
+face global bullet    ${rgb(colors.syntax.markup)}
+face global list      ${rgb(colors.syntax.markup)}
 
 # builtin
-face global Error              default,${rgb(colors.syntax.error)}
-face global Information        ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
-face global LineNumberCursor   ${rgb(colors.ui.line)},${rgb(colors.common.accent)}
-face global LineNumbers        ${rgb(colors.ui.guide.normal)},${rgb(colors.ui.gutter.normal)}
-face global MatchingChar       default,${rgb(colors.common.bg)}
-face global MenuBackground     default,${rgb(colors.ui.panel.bg)}
-face global MenuForeground     ${rgb(colors.ui.panel.bg)},${rgb(colors.common.fg)}
-face global MenuInfo           ${rgb(colors.common.fg)}
-face global PrimaryCursor      ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
-face global PrimaryCursorEol   ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
-face global PrimarySelection   ${rgb(colors.common.fg)},${rgb(colors.ui.selection.bg)}
-face global Prompt             ${rgb(colors.common.accent)}
-face global SecondaryCursor    ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
-face global SecondaryCursorEol ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
-face global SecondarySelection ${rgb(colors.common.fg)},${rgb(colors.ui.selection.bg)}
-face global StatusCursor       ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
-face global StatusLine         ${rgb(colors.common.fg)},${rgb(colors.ui.panel.bg)}
-face global StatusLineInfo     ${rgb(colors.syntax.tag)}
-face global StatusLineMode     ${rgb(colors.common.accent)}+b
-face global StatusLineValue    ${rgb(colors.syntax.error)}`
+face global Default ${rgb(colors.common.fg)},${rgb(colors.common.bg)}
+face global Error   ${rgb(colors.syntax.error)}
+
+face global PrimarySelection   default,${rgb(colors.ui.selection.bg.brighten(0.3))}
+face global SecondarySelection default,${rgb(colors.ui.selection.inactive)}
+face global PrimaryCursor      ${rgb(colors.common.bg)},${rgb(colors.common.accent)}
+face global SecondaryCursor    ${rgb(colors.common.bg)},${rgb(colors.common.accent.fade(0.3))}
+face global PrimaryCursorEol   ${rgb(colors.common.bg)},${rgb(colors.common.accent.fade(0.5))}
+face global SecondaryCursorEol ${rgb(colors.common.bg)},${rgb(colors.common.accent.fade(0.8))}
+
+face global MenuForeground ${rgb(colors.common.fg)},${rgb(colors.ui.selection.bg)}+b
+face global MenuBackground ${rgb(colors.common.fg)},${rgb(colors.ui.selection.inactive)}
+face global MenuInfo       ${rgb(colors.common.fg)}+d
+
+face global Information ${rgb(colors.common.bg)},${rgb(colors.common.fg)}
+
+face global StatusLine      default,${rgb(colors.ui.selection.border)}
+face global StatusLineMode  ${rgb(colors.common.bg)},${rgb(colors.common.accent)}
+face global StatusLineInfo  ${rgb(colors.ui.selection.border)},${rgb(colors.common.fg)}
+face global StatusLineValue ${rgb(colors.common.bg)},${rgb(colors.common.accent)}
+face global Prompt          ${rgb(colors.common.accent)}
+# BuferPadding
+
+face global LineNumbers        ${rgb(colors.common.ui)}
+face global LineNumbersCursor  ${rgb(colors.common.ui)}
+face global LineNumbersWrapped ${rgb(colors.common.ui)}+b
+
+face global MatchingChar default,${rgb(colors.ui.selection.bg)}+b
+
+# Whitespace
+# WrapMarker`
   )
 })
