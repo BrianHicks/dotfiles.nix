96b0649c8dace1f0c0f836d0b84b15f3ab252dcc
Brian Hicks <brian@brianthicks.com>
2020-07-28 12:11:27 -0500

    add missing slash

--- dotfiles/zsh/rubofix.zsh
+++ dotfiles/zsh/rubofix.zsh
@@ -1,5 +1,5 @@
 # fix rubocop errors automatically
 function rubofix() {
     TOPLEVEL="$(git rev-parse --show-toplevel)"
-    git diff --name-only $(git rev-parse origin/master)...$(git rev-parse HEAD) | sed "s|^|$TOPLEVEL|g" | xargs rubocop --auto-correct
+    git diff --name-only $(git rev-parse origin/master)...$(git rev-parse HEAD) | sed "s|^|$TOPLEVEL/|g" | xargs rubocop --auto-correct
 }
