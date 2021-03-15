a4767f4d626b388c87b4f8d964007c8008a8484e
Brian Hicks <brian@brianthicks.com>
2021-03-15 06:25:40 -0500

    remove SSH control stuff

--- dotfiles/ssh.nix
+++ dotfiles/ssh.nix
@@ -4,10 +4,6 @@
   programs.ssh = {
     enable = true;
 
-    controlMaster = "auto";
-    controlPath = "~/.ssh/control/%r@%h:%p";
-    controlPersist = "5m";
-
     extraConfig = "Include config.d/*";
   };
 }
