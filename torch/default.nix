58c84052773f9ed71a77436ee338e620ec3dd982
Brian Hicks <brian@brianthicks.com>
2021-03-17 06:19:42 -0500

    default my shell to zsh

--- torch/default.nix
+++ torch/default.nix
@@ -41,6 +41,7 @@ in {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.brian = {
     isNormalUser = true;
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
