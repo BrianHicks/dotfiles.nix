b91297d804877b64b6568d265012825f07f7b1f8
Brian Hicks <brian@brianthicks.com>
2021-03-20 15:41:35 -0500

    bind keyboard volume keys to pactl commands

--- torch/sway.nix
+++ torch/sway.nix
@@ -6,6 +6,13 @@
       modifier = modifier;
       keybindings = lib.mkOptionDefault {
         "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+
+        # audio
+        "XF86AudioRaiseVolume" =
+          "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        "XF86AudioLowerVolume" =
+          "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
       };
 
       # this is the default and I'm trying it for now but I'm not completely sold
