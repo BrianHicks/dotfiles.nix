b4b16272d6efefeb4497c9bb0ad17b72988da268
Brian Hicks <brian@brianthicks.com>
2021-03-21 05:26:38 -0500

    control playerctl with keyboard buttons

--- torch/sway.nix
+++ torch/sway.nix
@@ -14,6 +14,9 @@
         "XF86AudioLowerVolume" =
           "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
         "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        "XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+        "XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next";
+        "XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous";
 
         # Brightness
         "XF86MonBrightnessDown" = "exec brillo -U 5";
