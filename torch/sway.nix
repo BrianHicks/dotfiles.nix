146d0a4cc9670f01d123bd4593b078327f0ba34e
Brian Hicks <brian@brianthicks.com>
2021-03-22 05:13:07 -0500

    just a little bigger, please

--- torch/sway.nix
+++ torch/sway.nix
@@ -59,7 +59,7 @@
     };
 
     extraConfig = ''
-      output "Apple Computer Inc Color LCD 0x00000000" scale 1.5
+      output "Apple Computer Inc Color LCD 0x00000000" scale 1.6
 
       # Start the pulseaudio unit so audio works when the system starts. TODO:
       # is this right? it feels like there's probably a better way to start it?
