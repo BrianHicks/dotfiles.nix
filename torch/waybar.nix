49a309d12376aa369d2fe4b3c2f4d609187791a6
Brian Hicks <brian@brianthicks.com>
2021-03-25 06:27:52 -0500

    use a really minimal waybar config

--- torch/waybar.nix
+++ torch/waybar.nix
@@ -1,6 +1,23 @@
 { ... }: {
   programs.waybar = {
     enable = true;
-    # settings = [ ];
+    settings = [{
+      layer = "top";
+
+      modules-left = [ "sway/workspaces" "sway/mode" ];
+      modules-center = [ "sway/window" ];
+      modules-right = [ "battery" "clock" ];
+
+      modules = {
+        "sway/window".max-length = 50;
+
+        battery = {
+          format = "{capacity}% {icon}";
+          format-icons = [ "" "" "" "" "" ];
+        };
+
+        clock.format = "{:%a, %d. %b  %H:%M}";
+      };
+    }];
   };
 }
