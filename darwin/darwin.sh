62afaae3773f4d18419100223455421cf8b33348
Brian Hicks <brian@brianthicks.com>
2021-06-29 16:34:11 -0500

    fix the default.nix path

--- darwin/darwin.sh
+++ darwin/darwin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p moreutils
 HERE="$(realpath $(dirname $0))"
-darwin-rebuild $@ -I "darwin-config=$HERE/darwin/default.nix"
+darwin-rebuild $@ -I "darwin-config=$HERE/default.nix"
