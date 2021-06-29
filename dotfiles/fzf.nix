1991a7b23c71dbd51181a684c3026e62942a7e4c
Brian Hicks <brian@brianthicks.com>
2021-06-29 17:03:12 -0500

    improve file+change dir widgets in fzf

--- dotfiles/fzf.nix
+++ dotfiles/fzf.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:
 
 {
-  programs.fzf = {
+  programs.fzf = rec {
     enable = true;
     enableZshIntegration = true;
+
+    defaultCommand = "${pkgs.fd}/bin/fd --type f";
+
+    fileWidgetCommand = defaultCommand;
+    fileWidgetOptions = [
+      "--preview '${pkgs.bat}/bin/bat --color=always --paging=never --style=changes {}' --preview-window down"
+    ];
+
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+    changeDirWidgetOptions =
+      [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
   };
 }
