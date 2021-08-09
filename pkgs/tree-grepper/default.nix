090f22a3d8c7bddb81576f56393e2481d9f2419b
Brian Hicks <brian@brianthicks.com>
2021-08-09 08:36:53 -0500

    update sources

--- pkgs/tree-grepper/default.nix
+++ pkgs/tree-grepper/default.nix
@@ -3,7 +3,7 @@
 pkgs.callPackage (pkgs.fetchgit {
   url = sources.tree-grepper.repo;
   rev = sources.tree-grepper.rev;
-  sha256 = "11if9aw5mr10jmb25hn6003c1xxzjxgs8cs1z2g2r79rn0zx4nyw";
+  sha256 = "0w6ichhdxz0zlxpvdn1hl8a66gk265myxnz5zvxw81b2wj2k3yqw";
 
   # This whole thing is just a workaround for niv not being able to check
   # out a repo with submodules. No big deal to add a little wrapper, though!
