f2a8b9f8afab81a718a161ae0ecea1236ebad85f
Brian Hicks <brian@brianthicks.com>
2021-08-18 05:56:01 -0500

    this got renamed from gnome3 to just gnome

--- torch/configuration.nix
+++ torch/configuration.nix
@@ -49,7 +49,7 @@ inputs:
 
   # Enable the GNOME 3 Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
