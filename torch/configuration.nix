ef7465eabf972447473d10c0703a7dcfbe86c988
Brian Hicks <brian@brianthicks.com>
2021-08-19 05:24:19 -0500

    don't need proxy or i18n

--- torch/configuration.nix
+++ torch/configuration.nix
@@ -34,17 +34,6 @@ inputs:
   networking.useDHCP = false;
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
