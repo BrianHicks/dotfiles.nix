ef505e2f220218d4aa966a889b4a43429b0bad05
Brian Hicks <brian@brianthicks.com>
2021-08-19 05:31:50 -0500

    home-manager should be 21.05 to start, too

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-21.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
   };
 
   outputs = inputs: {
