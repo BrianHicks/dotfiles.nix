685329f995e37818c79635535adcd7785f4021a8
Brian Hicks <brian@brianthicks.com>
2021-09-01 05:15:44 -0500

    alphabetize items in the overlay

--- flake.nix
+++ flake.nix
@@ -82,30 +82,12 @@
         (final: prev:
           let naersk = inputs.naersk.lib."${system}";
           in {
-            tmux = prev.tmux.overrideAttrs (attrs:
-              attrs // {
-                src = inputs.tmux;
-
-                # macOS does some weird stuff with locales and character widths.
-                # Practically, that means that without extra support tmux will
-                # behave weirdly around multi-byte characters like emoji. Enabling
-                # utf8proc support should backfill the right tables so that tmux
-                # can get the correct character widths.
-                buildInputs = attrs.buildInputs ++ [ prev.utf8proc ];
-                configureFlags = attrs.configureFlags
-                  ++ [ "--enable-utf8proc" ];
-              });
-
-            git-gclone = final.callPackage ./pkgs/git-gclone { };
+            comma = final.callPackage inputs.comma { };
 
             # is this going to cause problems by not actually being a package?
             fzf-tab = inputs.fzf-tab;
 
-            lazygit-window = final.callPackage ./pkgs/lazygit-window { };
-
-            tmux-session = final.callPackage ./pkgs/tmux-session { };
-
-            comma = final.callPackage inputs.comma { };
+            git-gclone = final.callPackage ./pkgs/git-gclone { };
 
             kak-session = final.callPackage ./pkgs/kak-session { };
 
@@ -155,6 +137,24 @@
 
               tug = buildKakounePlugin "tug" inputs.tug;
             };
+
+            lazygit-window = final.callPackage ./pkgs/lazygit-window { };
+
+            tmux = prev.tmux.overrideAttrs (attrs:
+              attrs // {
+                src = inputs.tmux;
+
+                # macOS does some weird stuff with locales and character widths.
+                # Practically, that means that without extra support tmux will
+                # behave weirdly around multi-byte characters like emoji. Enabling
+                # utf8proc support should backfill the right tables so that tmux
+                # can get the correct character widths.
+                buildInputs = attrs.buildInputs ++ [ prev.utf8proc ];
+                configureFlags = attrs.configureFlags
+                  ++ [ "--enable-utf8proc" ];
+              });
+
+            tmux-session = final.callPackage ./pkgs/tmux-session { };
           })
       ];
     in {
