e2c6036e15d255e34c0fdbf6a03583f12e25e1a0
Brian Hicks <brian@brianthicks.com>
2021-09-05 05:14:10 -0500

    make home-manager follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-21.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+
     home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
