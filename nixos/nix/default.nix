8bf354a7cc79deecbf6ff5d49b2980eb89c7692b
Brian Hicks <brian@brianthicks.com>
2021-09-05 06:07:10 -0500

    this turned out to not be true

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -1,7 +1,6 @@
 inputs:
 { pkgs, ... }: {
   nix = {
-    # TODO: might have to disable this to get home-manager working
     package = pkgs.nixUnstable;
     extraOptions = ''
       experimental-features = nix-command flakes
