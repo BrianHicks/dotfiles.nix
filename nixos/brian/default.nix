305df0036e2ad40b178c984ab546b3367d414fee
Brian Hicks <brian@brianthicks.com>
2021-09-07 05:46:53 -0500

    set up borg backups

--- nixos/brian/default.nix
+++ nixos/brian/default.nix
@@ -15,4 +15,39 @@
     useUserPackages = true;
     users.brian = import ../../dotfiles;
   };
+
+  services.borgbackup.jobs.brian-home = {
+    user = "brian";
+    group = "";
+
+    repo = "ipi51989@ipi51989.repo.borgbase.com:repo";
+
+    startAt = "hourly";
+
+    paths = "/home/brian";
+    exclude = [
+      "/home/*/.cache"
+      "/home/*/.cargo"
+      "/home/*/.compose-cache"
+      "/home/*/.config/chromium"
+      "/home/*/.dropbox"
+      "/home/*/Dropbox"
+    ];
+    compression = "auto,zlib,6";
+
+    encryption.mode = "repokey-blake2";
+    encryption.passCommand =
+      "cat /home/brian/.ssh/borgbase-brian-home-encryption-key";
+
+    environment.BORG_RSH = "ssh -i /home/brian/.ssh/borgbase_ed25519";
+
+    extraCreateArgs = "--stats";
+
+    prune.keep = {
+      within = "1d";
+      daily = 7;
+      weekly = 4;
+      monthly = -1; # keep one from each month
+    };
+  };
 }
