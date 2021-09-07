4ef44f87e055db141cb04cbf2b5f07651f79ddce
Brian Hicks <brian@brianthicks.com>
2021-09-07 17:19:25 -0500

    set stateVersion in dotfiles

--- dotfiles/home-manager/default.nix
+++ dotfiles/home-manager/default.nix
@@ -4,4 +4,7 @@
 
   manual.html.enable = true; # adds home-manager-help
   manual.manpages.enable = true;
+
+  # Note: this probably doesn't need to change very often!
+  home.stateVersion = "21.05";
 }
