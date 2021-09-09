cba00b199e6ca228f07022e1107c5aa3b1ef8e8f
Brian Hicks <brian@brianthicks.com>
2021-09-09 06:26:46 -0500

    re-enable the lsp configuration for Rust

--- dotfiles/kakoune/default.nix
+++ dotfiles/kakoune/default.nix
@@ -276,21 +276,21 @@ in {
 
         map buffer normal <a-minus> ': outline-jump-rust<ret>'
 
-        # lsp-enable-window
+        lsp-enable-window
 
-        # hook window -group rust-inlay-hints BufReload .* rust-analyzer-inlay-hints
-        # hook window -group rust-inlay-hints NormalIdle .* rust-analyzer-inlay-hints
-        # hook window -group rust-inlay-hints InsertIdle .* rust-analyzer-inlay-hints
-        # hook -once -always window WinSetOption filetype=.* %{
-        #   remove-hooks window rust-inlay-hints
-        # }
+        hook window -group rust-inlay-hints BufReload .* rust-analyzer-inlay-hints
+        hook window -group rust-inlay-hints NormalIdle .* rust-analyzer-inlay-hints
+        hook window -group rust-inlay-hints InsertIdle .* rust-analyzer-inlay-hints
+        hook -once -always window WinSetOption filetype=.* %{
+          remove-hooks window rust-inlay-hints
+        }
 
-        # hook window -group semantic-tokens BufReload .* lsp-semantic-tokens
-        # hook window -group semantic-tokens NormalIdle .* lsp-semantic-tokens
-        # hook window -group semantic-tokens InsertIdle .* lsp-semantic-tokens
-        # hook -once -always window WinSetOption filetype=.* %{
-        #   remove-hooks window semantic-tokens
-        # }
+        hook window -group semantic-tokens BufReload .* lsp-semantic-tokens
+        hook window -group semantic-tokens NormalIdle .* lsp-semantic-tokens
+        hook window -group semantic-tokens InsertIdle .* lsp-semantic-tokens
+        hook -once -always window WinSetOption filetype=.* %{
+          remove-hooks window semantic-tokens
+        }
       }
 
       hook global WinSetOption filetype=sh %{
