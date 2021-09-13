58e4250a5f747927cd8d8c878ee130993e8071c7
Brian Hicks <brian@brianthicks.com>
2021-09-13 05:59:43 -0500

    brignt over brightness controls

--- nixos/i3/default.nix
+++ nixos/i3/default.nix
@@ -40,6 +40,12 @@
       bindsym XF86AudioLowerVolume exec --no-startup-id ${pkgs.alsaUtils}/bin/amixer sset Master 5%- && $refresh_i3status
       bindsym XF86AudioMute exec --no-startup-id ${pkgs.alsaUtils}/bin/amixer sset Master toggle && $refresh_i3status
 
+      # Use brightnessctl to adjust screen + key brightness
+      bindsym XF86MonBrightnessUp --no-startup-id ${pkgs.brightnessctl}/bin/brightnessctl -d intel_backlight set 5%+
+      bindsym XF86MonBrightnessDown --no-startup-id ${pkgs.brightnessctl}/bin/brightnessctl -d intel_backlight set 5%-
+      bindsym XF86KbdBrightnessUp --no-startup-id ${pkgs.brightnessctl}/bin/brightnessctl -d smc::kbd_backlight set 5%+
+      bindsym XF86KbdBrightnessDown --no-startup-id ${pkgs.brightnessctl}/bin/brightnessctl -d smc::kbd_backlight set 5%-
+
       # Use Mouse+$mod to drag floating windows to their wanted position
       floating_modifier $mod
 
