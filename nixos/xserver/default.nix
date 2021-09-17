18619b76b69f167328f7a41bb4d3089ff5fbeb5d
Brian Hicks <brian@brianthicks.com>
2021-09-17 05:51:29 -0500

    fade between windows quicker

--- nixos/xserver/default.nix
+++ nixos/xserver/default.nix
@@ -26,6 +26,7 @@
     fade = true;
     activeOpacity = 1.0;
     inactiveOpacity = 0.9;
+    fadeDelta = 3; # default 10
 
     # this was the thing that made the tearing go away!
     backend = "glx";
