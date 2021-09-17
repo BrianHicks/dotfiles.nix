ba09a2688a2e0558df7187f9481bd938bf762ecf
Brian Hicks <brian@brianthicks.com>
2021-09-17 06:26:51 -0500

    make alacritty always a little transparent

--- nixos/picom/default.nix
+++ nixos/picom/default.nix
@@ -8,6 +8,12 @@
     inactiveOpacity = 0.95;
     fadeDelta = 3; # default 10
 
+    opacityRules = [
+      # always make terminals slightly transparent
+      "95:class_g = 'Alacritty' && focused"
+      "90:class_g = 'Alacritty' && !focused"
+    ];
+
     # this was the thing that made the tearing go away!
     backend = "glx";
   };
