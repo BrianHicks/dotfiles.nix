d6a2d8742190560d3be1771a95f644bfcfeff256
Brian Hicks <brian@brianthicks.com>
2021-09-21 22:17:33 -0500

    don't need this dmenu thing

--- nixos/i3/default.nix
+++ nixos/i3/default.nix
@@ -59,9 +59,6 @@
         # bindsym $mod+d exec --no-startup-id dmenu_run
         # A more modern dmenu replacement is rofi:
         bindsym $mod+p exec "${pkgs.rofi}/bin/rofi --combi-modi window#drun -show combi -modi combi -show-icons"
-        # There also is i3-dmenu-desktop which only displays applications shipping a
-        # .desktop file. It is a wrapper around dmenu, so you need that installed.
-        # bindcode $mod+40 exec --no-startup-id i3-dmenu-desktop
 
         # change focus
         bindsym $mod+h focus left
