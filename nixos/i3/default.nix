2172c7b6a160413d933abebcfb857922111823ab
Brian Hicks <brian@brianthicks.com>
2021-09-21 22:17:14 -0500

    switch windows with rofi too

--- nixos/i3/default.nix
+++ nixos/i3/default.nix
@@ -58,7 +58,7 @@
         # start dmenu (a program launcher)
         # bindsym $mod+d exec --no-startup-id dmenu_run
         # A more modern dmenu replacement is rofi:
-        bindsym $mod+p exec "${pkgs.rofi}/bin/rofi -modi drun,run -show drun"
+        bindsym $mod+p exec "${pkgs.rofi}/bin/rofi --combi-modi window#drun -show combi -modi combi -show-icons"
         # There also is i3-dmenu-desktop which only displays applications shipping a
         # .desktop file. It is a wrapper around dmenu, so you need that installed.
         # bindcode $mod+40 exec --no-startup-id i3-dmenu-desktop
