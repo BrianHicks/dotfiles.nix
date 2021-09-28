6988d3c022ed0d68210f38d1c30374556a3e3a24
Brian Hicks <brian@brianthicks.com>
2021-09-28 14:44:59 -0500

    add keys to SSH agent

--- dotfiles/ssh/default.nix
+++ dotfiles/ssh/default.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   programs.ssh = {
     enable = true;
-    extraConfig = "Include config.d/*";
+    extraConfig = ''
+      AddKeysToAgent yes
+      Include config.d/*
+    '';
   };
 }
