9731d3cca786233bb049b903088e40bf687c6630
Brian Hicks <brian@brianthicks.com>
2021-11-18 14:55:36 -0600

    add a delete keybinding to lf

--- dotfiles/lf/default.nix
+++ dotfiles/lf/default.nix
@@ -1,1 +1,10 @@
-{ ... }: { programs.lf.enable = true; }
+{ ... }: {
+  programs.lf = {
+    enable = true;
+    keybindings = {
+      # I find x to be a better cut, and save d for delete
+      d = "delete";
+      x = "cut";
+    };
+  };
+}
