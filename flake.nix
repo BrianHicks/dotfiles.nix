6ab59338cdad24bef0cf5bc7ceff2cd8ba7c0f1a
Brian Hicks <brian@brianthicks.com>
2021-12-15 15:13:50 -0600

    upgrade to 21.11 everywhere

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Brian's Dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin";
 
@@ -11,7 +11,7 @@
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     naersk.url = "github:nix-community/naersk";
