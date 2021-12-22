be328719e3a0e9e4e2ee23dc2fefab6cafcdefac
Brian Hicks <brian@brianthicks.com>
2021-12-22 14:25:02 -0600

    abstract grid math to a function

--- dotfiles/hammerspoon/default.nix
+++ dotfiles/hammerspoon/default.nix
@@ -14,6 +14,18 @@
       win:setFrame(windowFrame)
     end
 
+    function resizeToCursorScreenOnGrid(gridColumns, gridRows, xOffset, yOffset, width, height)
+      resizeToCursorScreen(function(windowFrame, screenFrame)
+        local segmentsW = screenFrame.w / gridColumns
+        local segmentsH = screenFrame.h / gridRows
+
+        windowFrame.x = screenFrame.x + xOffset * segmentsW
+        windowFrame.y = screenFrame.y + yOffset * segmentsH
+        windowFrame.w = segmentsW * width
+        windowFrame.h = segmentsH * height
+      end)
+    end
+
     hs.hotkey.bind(hyper, "F", function()
       resizeToCursorScreen(function(windowFrame, screenFrame)
         windowFrame.x = screenFrame.x
@@ -24,137 +36,35 @@
     end)
 
     hs.hotkey.bind(hyper, "M", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      local segments = 6
-      local segmentW = max.w / segments
-      local segmentH = max.h / segments
-
-      f.x = max.x + segmentW
-      f.y = max.y + segmentH
-      f.w = segmentW * 4
-      f.h = segmentH * 4
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(6, 6, 1, 1, 4, 4)
     end)
 
     hs.hotkey.bind(hyper, "Z", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      local segments = 6
-      local segmentW = max.w / segments
-      local segmentH = max.h / segments
-
-      f.x = max.x + segmentW
-      f.y = max.y
-      f.w = segmentW * 4
-      f.h = segmentH * 5
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(6, 6, 1, 0, 4, 5)
     end)
 
     hs.hotkey.bind(hyper, "S", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      local segments = 6
-      local segmentH = max.h / segments
-
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w
-      f.h = segmentH * 4
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(6, 6, 0, 0, 6, 4)
     end)
 
     hs.hotkey.bind(hyper, "T", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      local segments = 6
-      local segmentH = max.h / segments
-
-      f.x = max.x
-      f.y = max.y + segmentH * 4
-      f.w = max.w
-      f.h = segmentH * 2
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(6, 6, 0, 4, 6, 2)
     end)
 
     hs.hotkey.bind(hyper, "Left", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w / 2
-      f.h = max.h
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(2, 1, 0, 0, 1, 1)
     end)
 
     hs.hotkey.bind(hyper, "Right", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      f.x = max.x + max.w / 2
-      f.y = max.y
-      f.w = max.w / 2
-      f.h = max.h
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(2, 1, 1, 0, 1, 1)
     end)
 
     hs.hotkey.bind(hyper, "Up", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w
-      f.h = max.h / 2
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(1, 2, 0, 0, 1, 1)
     end)
 
     hs.hotkey.bind(hyper, "Down", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = hs.mouse.getCurrentScreen()
-      local max = screen:frame()
-
-      f.x = max.x
-      f.y = max.y + max.h / 2
-      f.w = max.w
-      f.h = max.h / 2
-
-      win:moveToScreen(screen)
-      win:setFrame(f)
+      resizeToCursorScreenOnGrid(1, 2, 0, 1, 1, 1)
     end)
 
     hs.hotkey.bind(hyper, "C", function()
