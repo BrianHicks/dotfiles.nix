0de4297b41d7fd5e6c6495e57b8c05fb7e4f5693
Brian Hicks <brian@brianthicks.com>
2021-12-22 13:51:19 -0600

    make f and m move to the cursor's screen

--- dotfiles/hammerspoon/default.nix
+++ dotfiles/hammerspoon/default.nix
@@ -5,20 +5,22 @@
     hs.hotkey.bind(hyper, "F", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
-      local screen = win:screen()
+      local screen = hs.mouse.getCurrentScreen()
       local max = screen:frame()
 
       f.x = max.x
       f.y = max.y
       f.w = max.w
       f.h = max.h
+
+      win:moveToScreen(screen)
       win:setFrame(f)
     end)
 
     hs.hotkey.bind(hyper, "M", function()
       local win = hs.window.focusedWindow()
       local f = win:frame()
-      local screen = win:screen()
+      local screen = hs.mouse.getCurrentScreen()
       local max = screen:frame()
 
       local segments = 6
@@ -29,6 +31,8 @@
       f.y = max.y + segmentH
       f.w = segmentW * 4
       f.h = segmentH * 4
+
+      win:moveToScreen(screen)
       win:setFrame(f)
     end)
 
