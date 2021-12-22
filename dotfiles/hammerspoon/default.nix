ff871638ab21ee54b53b7e97a5a5c824555f328e
Brian Hicks <brian@brianthicks.com>
2021-12-22 13:57:13 -0600

    add a z hotkey

--- dotfiles/hammerspoon/default.nix
+++ dotfiles/hammerspoon/default.nix
@@ -36,6 +36,25 @@
       win:setFrame(f)
     end)
 
+    hs.hotkey.bind(hyper, "Z", function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = hs.mouse.getCurrentScreen()
+      local max = screen:frame()
+
+      local segments = 6
+      local segmentW = max.w / segments
+      local segmentH = max.h / segments
+
+      f.x = max.x + segmentW
+      f.y = max.y
+      f.w = segmentW * 4
+      f.h = segmentH * 5
+
+      win:moveToScreen(screen)
+      win:setFrame(f)
+    end)
+
     hs.hotkey.bind(hyper, "C", function()
       local win = hs.window.focusedWindow()
       local screen = hs.mouse.getCurrentScreen()
