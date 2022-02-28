5366ad084014d500e182aa8bb2f52497419edc4b
Brian Hicks <brian@brianthicks.com>
2022-02-28 11:17:45 -0600

    use enter for open too

--- dotfiles/lf/default.nix
+++ dotfiles/lf/default.nix
@@ -5,6 +5,7 @@
       # I find x to be a better cut, and save d for delete
       d = "delete";
       x = "cut";
+      "<enter>" = "open";
     };
 
     previewer.source = "${pkgs.pistol}/bin/pistol";
