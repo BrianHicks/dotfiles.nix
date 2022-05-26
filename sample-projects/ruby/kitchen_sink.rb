421630f8726141db5a9779e012ed7ba97cc05a27
Brian Hicks <brian@brianthicks.com>
2022-05-26 05:55:36 -0500

    add block parameters to name finder

---
+++ sample-projects/ruby/kitchen_sink.rb
@@ -0,0 +1,8 @@
+# stuff that I notice is missing from my tools, mostly, rather than an attempt to
+# be exhaustive.
+
+{ a: 1, b: 2 }.each do |k, v|
+  puts "#{k} #{v}"
+end
+
+{ c: 1, d: 2 }.each { |k, v| puts "#{k} #{v}" }
