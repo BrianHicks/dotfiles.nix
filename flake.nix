52f49a4b8d5b858fddb737168ea6488d608846b1
Brian Hicks <brian@brianthicks.com>
2023-04-05 08:36:52 -0500

    track home-manager against nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     naersk.url = "github:nix-community/naersk";
