0402ceefae5bb29b136c75ef4f43cc9d07954436
Brian Hicks <brian@brianthicks.com>
2023-06-29 15:12:15 -0500

    set leaders immediately

--- dotfiles/neovim/default.nix
+++ dotfiles/neovim/default.nix
@@ -3,5 +3,12 @@
   programs.neovim = {
     enable = true;
     vimAlias = true;
+
+    extraLuaConfig = ''
+      -- Leaders need to be mapped before requiring anything so they will be
+      -- correctly bound everywhere.
+      vim.g.mapleader = ' '
+      vim.g.maplocalleader = ' '
+    '';
   };
 }
