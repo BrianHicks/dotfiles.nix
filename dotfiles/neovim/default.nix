4cb9e9e6cf1e296eb4d477cf05b9c2ce75c45839
Brian Hicks <brian@brianthicks.com>
2023-06-30 14:07:33 -0500

    oops, that's not the name

--- dotfiles/neovim/default.nix
+++ dotfiles/neovim/default.nix
@@ -80,7 +80,7 @@
 
       -- telescope
       vim.keymap.set('n', '-', ':Telescope find_files<CR>', { desc = 'find files' })
-      vim.keymap.set('n', '_', ':Telescope find_buffers<CR>', { desc = '[F]ind [B]uffers' })
+      vim.keymap.set('n', '_', ':Telescope buffers<CR>', { desc = '[F]ind [B]uffers' })
       vim.keymap.set('n', '<leader>ff', ':Telescope resume<CR>', { desc = 'Resume last Telescope operation' })
       vim.keymap.set('n', '<leader>f/', ':Telescope current_buffer_fuzzy_find<CR>', { desc = '[F]ind by searching' })
       vim.keymap.set('n', '<leader>fp', ':Telescope live_grep<CR>', { desc = '[F]ind in [P]roject' })
