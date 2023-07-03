89cc3bba48ff7fac5fd3d53d68a1843afb9aa0ed
Brian Hicks <brian@brianthicks.com>
2023-07-03 17:05:54 -0500

    set up lsp stuff in buffers

--- dotfiles/neovim/default.nix
+++ dotfiles/neovim/default.nix
@@ -95,6 +95,41 @@
       lspconfig.sorbet.setup({ cmd = { "srb", "typecheck", "--lsp", "--enable-all-beta-lsp-features" } })
       lspconfig.tsserver.setup({})
       lspconfig.nil_ls.setup({ cmd = { "${pkgs.nil}/bin/nil" } })
+
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+      vim.keymap.set('n', '<leader>D', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local nmap = function(keys, func, desc)
+            if desc then
+              desc = 'LSP: ' .. desc
+            end
+
+            vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = desc })
+          end
+ 
+          -- Buffer-local mappings
+          nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          nmap('<leader>a', vim.lsp.buf.code_action, 'Code [a]ction')
+
+          nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+          nmap('gr', ':Telescope lsp_references<CR>', '[G]oto [R]eferences')
+          nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+          nmap('gD', vim.lsp.buf.type_definition, '[G]oto type [D]efinition')
+          nmap('<leader>s', ':Telescope lsp_document_symbols<CR>', 'Document [S]ymbols')
+          nmap('<leader>S', ':Telescope lsp_dynamic_workspace_symbols<CR>', 'Workspace [S]ymbols')
+          nmap('K', vim.lsp.buf.hover, 'Hover documentation')
+          nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature documentation')
+
+          vim.api.nvim_buf_create_user_command(ev.buf, 'Format', function(_)
+            vim.lsp.buf.format()
+          end, { desc = 'Format current buffer with LSP' })
+       end,
+      })
     '';
 
     plugins = with pkgs.vimPlugins; [
