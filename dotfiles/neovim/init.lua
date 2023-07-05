e11e3877eaf27d316f781983160214717b332186
Brian Hicks <brian@brianthicks.com>
2023-07-04 23:19:51 -0500

    highlight on yank

--- dotfiles/neovim/init.lua
+++ dotfiles/neovim/init.lua
@@ -195,3 +195,15 @@ cmp.setup.cmdline(':', {
 
 -- status line
 require('lualine').setup()
+
+-- highlight on yank
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
