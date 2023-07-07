8e9dc659d363e20f5684a5addd0b70e2cfbebe07
Brian Hicks <brian@brianthicks.com>
2023-07-06 22:17:12 -0500

    add a next and previous trouble binding

--- dotfiles/neovim/trouble.lua
+++ dotfiles/neovim/trouble.lua
@@ -8,3 +8,7 @@ troubleBinding("<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>")
 troubleBinding("<leader>xl", "<cmd>TroubleToggle loclist<cr>")
 troubleBinding("<leader>xq", "<cmd>TroubleToggle quickfix<cr>")
 troubleBinding("gR", "<cmd>TroubleToggle lsp_references<cr>")
+
+local trouble = require("trouble")
+troubleBinding("]t", function(_) trouble.next({skip_groups = true, jump = true}) end)
+troubleBinding("[t", function(_) trouble.previous({skip_groups = true, jump = true}) end)
