2d770ab558d13a97e34006d285e08cc42822e91f
Brian Hicks <brian@brianthicks.com>
2023-07-06 22:17:05 -0500

    add a binding <c-t> to move telescope results to trouble

--- dotfiles/neovim/telescope.lua
+++ dotfiles/neovim/telescope.lua
@@ -1,3 +1,17 @@
+local actions = require("telescope.actions")
+local trouble = require("trouble.providers.telescope")
+
+local telescope = require("telescope")
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
+  },
+}
+
 vim.keymap.set('n', '<leader>f', ':Telescope find_files<CR>', { desc = 'find files' })
 vim.keymap.set('n', '<leader>b', ':Telescope buffers<CR>', { desc = '[F]ind [B]uffers' })
 vim.keymap.set('n', "<leader>'", ':Telescope resume<CR>', { desc = 'Resume last Telescope operation' })
