4980c40dc90b014fdc1f4156e9586499f3a54c35
Brian Hicks <brian@brianthicks.com>
2023-08-09 17:03:43 -0500

    tn, not tf

--- dotfiles/neovim/testing.lua
+++ dotfiles/neovim/testing.lua
@@ -1,7 +1,7 @@
 vim.g["test#strategy"] = "dispatch"
 
-vim.keymap.set('n', '<leader>tf', '<cmd>TestNearest<CR>', { desc = 'test nearest' })
 vim.keymap.set('n', '<leader>tt', '<cmd>TestFile<CR>', { desc = 'test file' })
+vim.keymap.set('n', '<leader>tn', '<cmd>TestNearest<CR>', { desc = 'test nearest' })
 vim.keymap.set('n', '<leader>ts', '<cmd>TestSuite<CR>', { desc = 'test suite' })
 vim.keymap.set('n', '<leader>tl', '<cmd>TestLast<CR>', { desc = 'test last' })
 vim.keymap.set('n', '<leader>to', '<cmd>TestVisit<CR>', { desc = 'test visit' })
