30db44a8fb3007d4017bf15958ec644bebd6450e
Brian Hicks <brian@brianthicks.com>
2023-10-12 09:42:55 -0500

    use <leader>` for finding from telescope sources

--- dotfiles/neovim/telescope.lua
+++ dotfiles/neovim/telescope.lua
@@ -31,4 +31,4 @@ vim.keymap.set('n', '<leader>b', '<cmd>Telescope buffers<CR>', { desc = '[F]ind 
 vim.keymap.set('n', "<leader>'", '<cmd>Telescope resume<CR>', { desc = 'Resume last Telescope operation' })
 vim.keymap.set('n', '<leader>l', '<cmd>Telescope current_buffer_fuzzy_find<CR>', { desc = '[F]ind by searching' })
 vim.keymap.set('n', '<leader>/', '<cmd>Telescope live_grep<CR>', { desc = '[F]ind in [P]roject' })
-vim.keymap.set('n', '<leader>s', '<cmd>Telescope builtin<CR>', { desc = '[F]ind [a]nything telescope can' })
+vim.keymap.set('n', '<leader>`', '<cmd>Telescope builtin<CR>', { desc = '[F]ind [a]nything telescope can' })
