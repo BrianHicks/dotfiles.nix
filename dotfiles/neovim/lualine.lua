df2fd196eb8b5a53669403e520387a38936cf7dd
Brian Hicks <brian@brianthicks.com>
2023-11-13 09:13:24 -0600

    have full filenames in lualine

--- dotfiles/neovim/lualine.lua
+++ dotfiles/neovim/lualine.lua
@@ -20,7 +20,12 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      }
+    },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -28,7 +33,12 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      }
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
