b8d867f49bb6bb1de013e1a48ea828f2b28a3755
Brian Hicks <brian@brianthicks.com>
2024-03-05 16:49:59 -0600

    add username and homeDirectory

--- home.nix
+++ home.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   home.stateVersion = "23.11";
 
+  home.username = "brianhicks";
+  home.homeDirectory = "/Users/brianhicks";
+
   home.packages = [
     pkgs.xbar-pr-status
     pkgs.xbar-review-request-status
