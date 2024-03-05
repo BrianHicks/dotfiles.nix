846e3d881371e4e20143d5c464e242699f4a0a4f
Brian Hicks <brian@brianthicks.com>
2024-03-05 16:47:44 -0600

    remove conflicting stateVersion

--- dotfiles/home-manager/default.nix
+++ dotfiles/home-manager/default.nix
@@ -4,7 +4,4 @@
 
   manual.html.enable = true; # adds home-manager-help
   manual.manpages.enable = true;
-
-  # Note: this probably doesn't need to change very often!
-  home.stateVersion = "21.05";
 }
