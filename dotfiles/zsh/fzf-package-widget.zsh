768388573bbf9123b596e95beb34d5bbf1f863c1
Brian Hicks <brian@brianthicks.com>
2024-03-12 10:46:40 -0500

    make the fzf-package-widget a little nicer

--- dotfiles/zsh/fzf-package-widget.zsh
+++ dotfiles/zsh/fzf-package-widget.zsh
@@ -4,7 +4,7 @@ fzf-package-widget() {
   local project_root="$(git rev-parse --show-toplevel)"
   local project_packages="$(find "$project_root" -maxdepth 3 -name 'package.json' | grep -v "${project_root}/package.json" | sed -E 's|/package.json$||g' | sed -E "s|${project_root}/(.+)|\0:\1|")"
 
-  local package_dest="$(fzf --delimiter : --with-nth 2 <<< "$project_packages" | cut -d : -f 1)"
+  local package_dest="$(fzf --delimiter : --with-nth 2 <<< "$project_packages" --height 40% --reverse --scheme=path | cut -d : -f 1)"
 
   if test -z "$package_dest"; then
     zle redisplay
