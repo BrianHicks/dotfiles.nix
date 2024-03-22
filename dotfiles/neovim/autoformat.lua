8b9ed0ca6695942460941fc74196ed568159d0fb
Brian Hicks <brian@brianthicks.com>
2024-03-22 17:28:39 -0500

    add auto-formatting

--- dotfiles/neovim/autoformat.lua
+++ dotfiles/neovim/autoformat.lua
@@ -38,3 +38,11 @@ require("formatter").setup {
     }
   }
 }
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
