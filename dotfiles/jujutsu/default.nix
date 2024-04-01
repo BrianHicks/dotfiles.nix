5484f2793c81af5697b1b6a00bcb5ab2cf372150
Brian Hicks <brian@brianthicks.com>
2024-04-01 11:30:29 -0500

    configure name and email for jj

--- dotfiles/jujutsu/default.nix
+++ dotfiles/jujutsu/default.nix
@@ -1,3 +1,10 @@
 { ... }: {
-  programs.jujutsu.enable = true;
+  programs.jujutsu = {
+    enable = true;
+
+    settings = {
+      user.name = "Brian Hicks";
+      user.email = "brian@brianthicks.com";
+    };
+  };
 }
