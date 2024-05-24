ec6fce15fa00673d4da800939e9fb1d4f00fa569
Brian Hicks <brian@brianthicks.com>
2024-05-24 17:30:12 -0500

    drop nixos-hardware input

--- flake.nix
+++ flake.nix
@@ -6,8 +6,6 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
