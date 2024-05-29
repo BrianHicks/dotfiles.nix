b502ec22e04662c1003fa7f9572e73aa41c3fcef
Brian Hicks <brian@brianthicks.com>
2024-05-29 16:55:27 -0500

    add builders

--- dotfiles/nixconfig/default.nix
+++ dotfiles/nixconfig/default.nix
@@ -2,5 +2,8 @@
   home.file.".config/nix/nix.conf".text = ''
     substituters = https://cache.nixos.org https://cache.nixos.org/ https://brianhicks-dotfiles.cachix.org
     trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= brianhicks-dotfiles.cachix.org-1:TxOMdKYcrai6qqP2IXJcvcPChipzP2GgPfI1DxFasIM=
+
+    builders = "ssh://eu.nixbuild.net x86_64-linux - 100 1 big-parallel,benchmark"
+    builders-use-substitutes = true
   '';
 }
