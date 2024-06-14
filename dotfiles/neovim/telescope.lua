67a63daa963b92795b7e74ed03d2944899d9296f
Brian Hicks <brian@brianthicks.com>
2024-06-13 19:30:13 -0500

    upgrade deprecated call

--- dotfiles/neovim/telescope.lua
+++ dotfiles/neovim/telescope.lua
@@ -1,13 +1,13 @@
 local actions = require("telescope.actions")
-local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 
 local telescope = require("telescope")
 
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ["<c-t>"] = trouble.open_with_trouble },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
+      i = { ["<c-t>"] = trouble.open },
+      n = { ["<c-t>"] = trouble.open },
     },
   },
   extensions = {
