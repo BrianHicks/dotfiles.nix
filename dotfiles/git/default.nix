0ad5177f8fe8c89a452c3df9a956e060b0de7fe6
Brian Hicks <brian@brianthicks.com>
2025-12-05 22:37:52 -0600

    reenable git

---
+++ dotfiles/git/default.nix
@@ -0,0 +1,94 @@
+{ pkgs, ... }: {
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user = {
+         name = "Brian Hicks";
+         email = "brian@brianthicks.com";
+      };
+
+      alias = {
+        aa = "add --all";
+        amend = "commit --amend";
+        ci = "commit";
+        co = "checkout";
+        dc = "diff --cached";
+        di = "diff";
+        st = "status";
+        yoda = "push --force-with-lease";
+        root = "rev-parse --show-toplevel";
+      };
+
+      core.fsmonitor = "true";
+
+      init.defaultBranch = "main";
+
+      # make `git push` automatically do `-u origin`
+      push.autoSetupRemote = true;
+
+      rerere.enabled = true;
+
+      "filter \"lfs\"" = {
+        clean = "${pkgs.git-lfs}/bin/git-lfs clean -- %f";
+        smudge = "${pkgs.git-lfs}/bin/git-lfs smudge -- %f";
+        process = "${pkgs.git-lfs}/bin/git-lfs filter-process";
+        required = true;
+      };
+    };
+
+    ignores = [ ".direnv" ".DS_Store" ];
+
+    # TODO: reenable
+    # difftastic = {
+    #   enable = true;
+    # };
+
+    signing = {
+      key = null;
+      signByDefault = true;
+
+      # The gpg2 binary provided by Nix doesn't work with the gpg paths
+      # provided by gpg-tools for Mac. Use the thing that's just on the PATH,
+      # although I'm not terribly happy with that.
+      signer = "gpg";
+    };
+  };
+
+  # home.file."${lazygit-config}".text = builtins.toJSON {
+  #   reporting = "off";
+  #   startupPopupVersion = 1;
+
+  #   update.method = "never"; # managed through nixpkgs
+
+  #   keybindings = {
+  #     universal.return = "q";
+  #     universal.createRebaseOptionsMenu = "M";
+  #     branches.mergeIntoCurrentBranch = "m";
+  #   };
+
+  #   gui.theme = {
+  #     lightTheme = false;
+  #     activeBorderColor = [ "green" "bold" ];
+  #     inactiveBorderColor = [ "white" ];
+  #     optionsTextColor = [ "blue" ];
+
+  #     # the default highlighted color is a very bright blue that doesn't have
+  #     # sufficient contrast with the foreground.
+  #     selectedLineBgColor = [ "reverse" ];
+  #     selectedRangeBgColor = [ "black" "bold" ];
+  #   };
+
+  #   git.paging = {
+  #     colorArg = "always";
+  #     externalDiffCommand = "${pkgs.difftastic}/bin/difft --color=always";
+  #   };
+
+  #   git.overrideGpg = true; # prevents spawning a separate process on commits
+
+  #   # my SSH agent (1password) currently spams auth prompts when using lazygit, so
+  #   # I'm turning off auto-fetch stuff, hopefully temporarily!
+  #   git.autoFetch = false;
+  #   git.autoRefresh = false;
+  # };
+}
