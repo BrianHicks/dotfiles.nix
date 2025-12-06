7195105ae4f004eb8459409fa5de0bf9fe2c9400
Brian Hicks <brian@brianthicks.com>
2025-12-06 07:29:39 -0600

    set nix-command and flakes

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  home.file.".config/nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+  '';
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "brianhicks";
