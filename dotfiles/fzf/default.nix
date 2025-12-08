ded0d74c9c2bd9a55304d530afbe10a77938040d
Brian Hicks <brian@brianthicks.com>
2025-12-08 06:33:32 -0600

    add fzf

---
+++ dotfiles/fzf/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  programs.fzf = rec {
+    enable = true;
+    enableZshIntegration = true;
+
+    defaultCommand = "${pkgs.fd}/bin/fd";
+
+    fileWidgetCommand = defaultCommand;
+    fileWidgetOptions = [
+      "--preview '${pkgs.bat}/bin/bat --color=always --paging=never --style=changes {}' --preview-window down"
+    ];
+
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+    changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+  };
+}
