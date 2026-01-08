2af9552434481e36373fce86fecb68418e2f10c3
Brian Hicks <brian@brianthicks.com>
2026-01-08 14:32:33 -0600

    add jj aliases

--- dotfiles/jj/default.nix
+++ dotfiles/jj/default.nix
@@ -6,6 +6,67 @@
     settings = {
       user.name = config.programs.git.settings.user.name;
       user.email = config.programs.git.settings.user.email;
+
+      aliases = {
+        # Aliases from Artem that haven't entered my muscle memory yet
+        log-recent = [
+          "log"
+          "-r"
+          "default() & recent()"
+        ];
+        tug = [
+          "bookmark"
+          "move"
+          "--from"
+          "closest_bookmark(@-)"
+          "--to"
+          "@-"
+        ];
+        c = [ "commit" ];
+        ci = [
+          "commit"
+          "--interactive"
+        ];
+        e = [ "edit" ];
+        i = [
+          "git"
+          "init"
+          "--colocate"
+        ];
+        nb = [
+          "bookmark"
+          "create"
+          "-r @-"
+        ];
+        pull = [
+          "git"
+          "fetch"
+        ];
+        push = [
+          "git"
+          "push"
+        ];
+        r = [ "rebase" ];
+        s = [ "squash" ];
+        si = [
+          "squash"
+          "--interactive"
+        ];
+
+        # from converations, but still not in muscle memory
+        unify = [
+          "new"
+          "-r"
+          "tracked_remote_bookmarks() & mine()"
+        ];
+        rebase-all = [
+          "rebase"
+          "-s"
+          "roots(trunk()..@)"
+          "-d"
+          "trunk()"
+        ];
+      };
     };
   };
 
