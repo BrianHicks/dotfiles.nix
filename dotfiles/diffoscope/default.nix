bfc6709de53c701cda021db05219c8b7853b0f56
Brian Hicks <brian@brianthicks.com>
2026-01-12 07:25:08 -0600

    don't run all the diffoscope tests

--- dotfiles/diffoscope/default.nix
+++ dotfiles/diffoscope/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.diffoscope ];
+  home.packages = [ (pkgs.diffoscope.overrideAttrs (old: { doCheck = false; })) ];
 
   home.shellAliases = {
     "dir-diff" = "diffoscope --exclude-directory-metadata=yes";
