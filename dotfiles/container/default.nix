fc29bdec41d7c226a6d60364a0a84c27e3a5963d
Brian Hicks <brian@brianthicks.com>
2026-05-28 11:47:27 -0500

    add socktainer

--- dotfiles/container/default.nix
+++ dotfiles/container/default.nix
@@ -1,4 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  homebrew.formulae = [ "container" ];
+  homebrew.taps = [ "socktainer/tap" ];
+  homebrew.formulae = [
+    "container"
+    "socktainer"
+  ];
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "container-config-preset" ''
+      container system property set build.rosetta true
+      container system property set build.cpus 8
+      container system property set build.memory 16G
+      container system property set container.cpus 8
+      container system property set container.memory 8G
+    '')
+  ];
 }
