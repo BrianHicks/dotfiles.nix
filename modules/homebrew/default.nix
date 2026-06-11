f7f496070cf02df91f17788a9f1029ff44f9d2f8
Brian Hicks <brian@brianthicks.com>
2026-06-11 18:11:31 -0500

    only run brew on darwin

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -18,7 +18,7 @@ with lib;
     default = [ ];
   };
 
-  config = mkIf (!(isNull config.homebrew)) {
+  config = mkIf (pkgs.stdenv.isDarwin && !(isNull config.homebrew)) {
     home.activation.installBrews = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       $DRY_RUN_CMD ${pkgs.coreutils}/bin/echo "Installing Homebrew packages..."
       $DRY_RUN_CMD ${pkgs.homebrew-sync}/bin/homebrew-sync --taps ${toString config.homebrew.taps} --formulae ${toString config.homebrew.formulae}
