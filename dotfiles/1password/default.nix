5510f24d34e4b2f27abbdf83297b38b228439d0a
Brian Hicks <brian@brianthicks.com>
2026-06-13 16:25:21 -0500

    stop tweaking nixpkgs on linux
    won't be possible soon, apparently

--- dotfiles/1password/default.nix
+++ dotfiles/1password/default.nix
@@ -5,7 +5,6 @@
     "1password-cli"
   ];
 
-  nixpkgs.config.allowUnfree = true;
   home.packages = lib.mkIf (!pkgs.stdenv.isDarwin) [
     pkgs._1password-gui
     pkgs._1password-cli
--- dotfiles/spotify/default.nix
+++ dotfiles/spotify/default.nix
@@ -2,6 +2,5 @@
 {
   homebrew.formulae = lib.mkIf pkgs.stdenv.isDarwin [ "spotify" ];
 
-  nixpkgs.config.allowUnfree = true;
   home.packages = lib.mkIf (!pkgs.stdenv.isDarwin) [ pkgs.spotify ];
 }
