88296aab0240bb2a1fb70eff0b595228a12394dd
Brian Hicks <brian@brianthicks.com>
2026-06-13 12:48:10 -0500

    set caps to ctrl (maybe)

--- machines/avior/default.nix
+++ machines/avior/default.nix
@@ -49,7 +49,7 @@
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.options = "caps:ctrl";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
