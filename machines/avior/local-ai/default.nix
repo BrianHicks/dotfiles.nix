41a5dea365336c6838f52e2eec29b4c3ec94d2a2
Brian Hicks <brian@brianthicks.com>
2026-06-17 06:15:40 -0500

    try librechat

--- machines/avior/local-ai/default.nix
+++ machines/avior/local-ai/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   services.llama-cpp = {
     enable = true;
     package = pkgs.ik-llama-cpp;
@@ -30,15 +30,40 @@
   # (20gb) just to be safe.
   systemd.services.llama-cpp.serviceConfig.LimitMEMLOCK = "20000000000";
 
-  services.open-webui = {
+  services.librechat = {
     enable = true;
-    host = "192.168.0.240";
-    openFirewall = true;
-    environment = {
-      # these are in the nixos option docs but not the open-webui docs
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
+    enableLocalDB = true;
+    credentialsFile = "/run/secrets/librechat"; # TODO: back up
+    env = {
+      HOST = "192.168.0.240";
+      PORT = 3080;
+      ALLOW_EMAIL_LOGIN = "true";
+      ALLOW_REGISTRATION = "true";
+    };
+    settings = {
+      version = "1.3.13";
+      cache = true;
+
+      endpoints.custom = [
+        {
+          name = "llama.cpp";
+          apiKey = "";
+          baseURL = "http://127.0.0.1:${toString config.services.llama-cpp.settings.port}/v1";
+          models = {
+            default = [ config.services.llama-cpp.settings.model ];
+            fetch = false;
+          };
+
+          titleConvo = true;
+          titleModel = "current_model";
+
+          modelDisplayLabel = "Qwen";
+        }
+      ];
+
+      registration.allowedDomains = [ "brianthicks.com" ];
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ 3080 ];
 }
