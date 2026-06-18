0627331215c560c3e00d88152af1a73639d38771
Brian Hicks <brian@brianthicks.com>
2026-06-18 16:34:26 -0500

    drop librechat entirely

--- machines/avior/local-ai/default.nix
+++ machines/avior/local-ai/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   services.llama-cpp = {
     enable = true;
     package = pkgs.ik-llama-cpp;
@@ -45,41 +45,6 @@
       ENABLE_FOLLOW_UP_GENERATION = "False";
       ENABLE_TITLE_GENERATION = "False";
       ENABLE_TAGS_GENERATION = "False";
-    };
-  };
-
-  services.librechat = {
-    enable = false; # enabling makes GHA build Mongo; heads up!
-    enableLocalDB = true;
-    credentialsFile = "/run/secrets/librechat"; # TODO: back up
-    env = {
-      HOST = "192.168.0.240";
-      PORT = 3080;
-      ALLOW_EMAIL_LOGIN = "true";
-      ALLOW_REGISTRATION = "true";
-    };
-    settings = {
-      version = "1.3.13";
-      cache = true;
-
-      endpoints.custom = [
-        {
-          name = "llama.cpp";
-          apiKey = "";
-          baseURL = "http://127.0.0.1:${toString config.services.llama-cpp.settings.port}/v1";
-          models = {
-            default = [ config.services.llama-cpp.settings.model ];
-            fetch = false;
-          };
-
-          titleConvo = true;
-          titleModel = "current_model";
-
-          modelDisplayLabel = "Qwen";
-        }
-      ];
-
-      registration.allowedDomains = [ "brianthicks.com" ];
     };
   };
 
