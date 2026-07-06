b4e87c26182ca8ed0865343c7b9c4e989b659aa3
Brian Hicks <brian@brianthicks.com>
2026-07-06 06:10:59 -0500

    rearrange options to avoid deprecation

--- dotfiles/fzf/default.nix
+++ dotfiles/fzf/default.nix
@@ -6,12 +6,16 @@
 
     defaultCommand = "${pkgs.fd}/bin/fd";
 
-    fileWidgetCommand = defaultCommand;
-    fileWidgetOptions = [
-      "--preview '${pkgs.bat}/bin/bat --color=always --paging=never --style=changes {}' --preview-window down"
-    ];
+    fileWidget = {
+      command = defaultCommand;
+      options = [
+        "--preview '${pkgs.bat}/bin/bat --color=always --paging=never --style=changes {}' --preview-window down"
+      ];
+    };
 
-    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
-    changeDirWidgetOptions = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+    changeDirWidget = {
+      command = "${pkgs.fd}/bin/fd --type d";
+      options = [ "--preview '${pkgs.tree}/bin/tree -C {} | head -200'" ];
+    };
   };
 }
