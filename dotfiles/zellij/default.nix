be51f1d66db0dc40e0cfb5cccff7ce5e2543ceff
Brian Hicks <brian@brianthicks.com>
2026-09-23 11:54:18 -0500

    configure zellij-autolock

--- dotfiles/zellij/default.nix
+++ dotfiles/zellij/default.nix
@@ -22,9 +22,43 @@ in
     };
 
     extraConfig = ''
+      // programs.zellij.plugins doesn't allow custom configuration as of
+      // 2026-09-23, but we need it to specify our list of special programs for
+      // autolock. That may change in the future... check it!
+      plugins {
+        autolock location="file:${pkgs.zellijPlugins.autolock}" {
+          is_enabled true
+          triggers "hx|jjui|lazygit|fzf"
+        }
+      }
+
+      load_plugins {
+        autolock
+      }
+
       keybinds {
-        shared_except "locked" {
+        shared {
+          bind "Alt z" { MessagePlugin "autolock" { payload "toggle"; }; }
+
+          // This is a bit provisional, but I *think* I want it even when
+          // locked (which practically means whenever I'm in Helix)
           bind "Alt g" { Run "${vcs-ui}" { floating true; close_on_exit true; }; }
+        }
+
+        locked {
+          // allow navigation even when locked
+          bind "Alt h" { MoveFocusOrTab "Left"; }
+          bind "Alt j" { MoveFocusOrTab "Up"; }
+          bind "Alt k" { MoveFocusOrTab "Down"; }
+          bind "Alt l" { MoveFocusOrTab "Right"; }
+        }
+
+        normal {
+          // Intercept "Enter" and pass through for autolock
+          bind "Enter" {
+            WriteChars "\u{000D}";
+            MessagePlugin "autolock" {};
+          }
         }
 
         tab {
